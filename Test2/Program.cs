76cceb3a26032320b1b3e3a70fb4e3ac331d867f
Shade-v2006 <Shade-v2006@yandex.ru>
2024-01-26 03:36:22 +0700

    add kod lesson1:test2

--- Test2/Program.cs
+++ Test2/Program.cs
@@ -1,8 +1,36 @@
-﻿int n = 4;
-int i = 0;
+﻿/*
+    Напишите программу, которая на вход принимает целое число N, а на выходе показывает все целые
+числа в промежутке от -N до N.
+Примеры:
+    4 => -4, -3, -2, -1, 0, 1, 2, 3, 4
+    2 => -2, -1, 0, 1, 2
+*/
 
-if (n>4)
+// Вариант 1:
+
+// int n = -4;
+
+// if (n < 0 )
+// {
+//     n = -n;
+// }
+// int i = -n;
+// while (i<=n)
+// {
+//     System.Console.Write(i);
+//     System.Console.Write(' ');
+//     i++;
+// }
+
+// Вариант 2:
+
+
+Console.WriteLine("Enter a number:  ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = -n; i <= n; i++)
 {
-    System.Console.WriteLine(i);
-    i=n-1;
+    System.Console.Write(i + " ");
 }
+
+Console.ReadKey();
